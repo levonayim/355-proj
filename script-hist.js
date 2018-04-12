ec128b0e81969bec63d7e04c845d1cf46045eed7
thomaslaurie <thomas.laurie@outlook.com>
2018-04-11 22:04:10 -0700

    save

--- script-hist.js
+++ script-hist.js
@@ -37,11 +37,11 @@ var z = d3.scaleOrdinal()
 //IMPORTING DATA
 d3.csv("table.csv",
 	function(d, i, columns) {
-		//start with 'Under $10,000' and since it's the 5th column, i = 4. t is the variable to count the total number of stacks it'll have. +d[columns[i]] converts it to workable integer
+		//start with 'Under $10,000' and since it's the 5th column, i = 4. t is the variable to count the total number of stacks it'll have. +d[columns[i]] converts it to string array
 		for (var i = 4, n = columns.length; i < n; ++i) {
 			d[columns[i]] = +d[columns[i]];
 		}
-			return d;
+		return d;
 	}, 
     //if something wrong with dataset, output error
     function(error, data) {
@@ -125,12 +125,14 @@ d3.csv("table.csv",
 d3.csv("table.csv",
 	function(d, i, columns) {
 		// brings in the actual data/numbers from the columns
-		//start with 'Under $10,000' and since it's the 5th column, i = 4. t is the variable to count the total number of stacks it'll have. t is add through the loop for each column of data, +d[columns[i]] converts it to string array
-		for (i = 4, t = 0; i < columns.length; ++i)
+		//start with 'Under $10,000' and since it's the 5th column, i = 4.
+		//t is the variable to count the total number of stacks it'll have.
+		//t is add through the loop for each column of data, +d[columns[i]] converts it to string array
+		for (i = 4, t = 0; i < columns.length; ++i) {
 			t += d[columns[i]] = +d[columns[i]];
 			d.total = t;
 		}
-			return d;	
+		return d;	
 	},
 
 	
@@ -213,3 +215,185 @@ d3.csv("table.csv",
 			.attr("dy", "0.32em")
 			.text(function(d) { return d; });
 });
+
+
+
+// old line graph stuff
+
+/*
+function iGToInt(iG) {
+	if (iG == 'Under $10,000') {
+		return 0;
+	} else if (iG == '$10,000 to $19,999') {
+		return 1;
+	} else if (iG == '$20,000 to $29,999') {
+		return 2;
+	} else if (iG == '$30,000 to $39,999') {
+		return 3;
+	} else if (iG == '$40,000 to $49,999') {
+		return 4;
+	} else if (iG == '$50,000 to $59,999') {
+		return 5;
+	} else if (iG == '$60,000 to $69,999') {
+		return 6;
+	} else if (iG == '$70,000 to $79,999') {
+		return 7;
+	} else if (iG == '$80,000 to $89,999') {
+		return 8;
+	} else if (iG == '$90,000 to $99,999') {
+		return 9;
+	} else if (iG == '$100,000 and over') {
+		return 10;
+	}   
+}
+
+// BOUNDS
+var margin = {top: 30, right: 50, bottom: 50, left: 70},
+	outerWidth = 550,
+	outerHeight = 550,
+	innerWidth = outerWidth - margin.left - margin.right,
+	innerHeight = outerHeight - margin.top - margin.bottom;
+
+// SCALE
+var x = d3.scale.linear().range([0, innerWidth]);
+var y = d3.scale.linear().range([innerHeight, 0]);
+
+// AXIS
+var xAxis = d3.svg.axis().scale(x).orient('bottom').ticks(5);
+var yAxis = d3.svg.axis().scale(y).orient('left').ticks(5);
+
+var color = d3.scale.category20();   // set the colour scale
+
+// ADD CHART (cannot be named svg or else would over lap it)
+var lineChart = d3.select('#area1')
+	.append('svg')
+	.attr('width', outerWidth)
+	.attr('height', outerHeight)
+		.append('g')
+		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+// Add the X Axis
+lineChart.append('g')
+	.attr('class', 'x axis')
+	.attr('transform', 'translate(0,' + outerHeight + ')')
+	.call(xAxis);
+
+// Add the Y Axis
+lineChart.append('g')
+	.attr('class', 'y axis')
+	.call(yAxis);
+
+
+d3.csv('table.csv', function(error, data) {
+	data.forEach(function(d) {
+		// CONVERT
+		d.name = d.name;
+		d.incomeGroup = d.incomeGroup;
+		d.houseType = d.houseType;
+		d.costIncomeRatio = d.costIncomeRatio;
+		// converts to number or 0
+		d.tenureOwner = +d.tenureOwner || 0;
+		d.tenureRenter = +d.tenureRenter || 0;
+		d.tenureBand = +d.tenureBand || 0;
+	});
+
+	// MAKE NESTED DATA
+	var dataNest = d3
+		// nest by name, then incomeGroup
+		.nest()
+		.key(function(d) { return d.name; })
+		.key(function(d) { return d.incomeGroup; })
+		// for each subgroup
+		.rollup(function(d) {
+			// return a set of calculated values
+			var total = d3.sum(d, function(d) { 
+				var total = d3.sum([d.tenureOwner, d.tenureRenter, d.tenureBand]);
+				return total;
+			});
+			var under30 = d3.sum(d, function(d) {
+				var total = d3.sum([d.tenureOwner, d.tenureRenter, d.tenureBand]);
+				if (d.costIncomeRatio == 'Spending less than 30% of income on shelter costs') {
+					return total;
+				} else {
+					return 0;
+				}
+			});
+			var ratio = under30 / total;
+			var percent = ratio * 100;
+
+			// as an object
+			return {
+				total: total,
+				under30: under30,
+				ratio: ratio,
+				percent: percent,
+			};
+		})
+		// supply dataset
+		.entries(data);
+	
+	// sort incomeGroups (so line draws in right order)
+	dataNest.forEach(function(d) {
+		d.values.sort(function(x, y) {
+			return d3.ascending(iGToInt(x.key), iGToInt(y.key));
+		});
+	});
+
+	// SCALE
+	// x, incomeGroup index, min to max
+	x.domain([
+		// min of
+		d3.min(dataNest, function(d) { 
+			// min of incomeGroup index
+			var n = d3.min(d.values, function(d) {
+				return iGToInt(d.key);
+			});
+			return n; 
+		}),
+		// to max of
+		d3.max(dataNest, function(d) { 
+			// max of incomeGroup index
+			var n = d3.max(d.values, function(d) {
+				return iGToInt(d.key);
+			});
+			return n; 
+		})
+	]);
+	// y, ratio, 0 to max
+	y.domain([0, 
+		// max of
+		d3.max(dataNest, function(d) { 
+			// max of incomeGroup percent
+			var n = d3.max(d.values, function(d) {
+				return d.values.percent;
+			});
+			return n; 
+		})
+	]);
+
+	// define line
+	var line = d3.svg.line() 
+		.x(function(d) { return x(iGToInt(d.key)); })
+		.y(function(d) { return y(d.values.percent); });
+
+	// for each entry
+	dataNest.forEach(function(d,i) { 
+		// append an svg <path>
+		lineChart.append('path')
+			// class='line'
+			.attr('class', 'line')
+			// ??? add colors dynamically
+			.style('stroke', function() { return d.color = color(d.key); })
+			// d='val, val, val'
+			.attr('d', line(d.values));
+	});
+
+	//HOVER OVER LINE. SHOW INFO. At the moment shows the 'highest' point of all the points for that city.. need to figure out how to show just the points.
+	lineChart.selectAll('path')
+		.data(data)
+		.append('title')
+		.text(function(d) {
+			return d.tenure + ' tenure ' + d.fraction + ' fraction' + d.income + ' income range' + d.GEO_NAME;
+		});
+});
+*/
